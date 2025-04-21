e7107c5e85222cec17732aa1eba482e23e90106b
BrenoCVS <brenocvsilva14@gmail.com>
2025-04-21 16:18:48 -0300

    21/04

--- main.java
+++ main.java
@@ -2,9 +2,9 @@ import javax.swing.JOptionPane;
 
 public class Main {
     private static String musica;
-    private static String [3][6] paginas;
+    private static String[][] paginas = new String[3][6];
     
-    public stativ void setMatriz(int linha, int coluna, String valor){
+    public static void setMatriz(int linha, int coluna, String valor){
         paginas[linha][coluna] = valor;
     } 
 
@@ -21,12 +21,20 @@ public class Main {
     }
 
     public static void main(String[] args) {
-         for (int i = 0; i <= 6 ; i++){
-            setMtriz(1, i, "Adicionar pagina " + i);
-        }
+        preencherMatriz();
 
         menu();
     }
+
+    public static void preencherMatriz(){
+         for (int i = 0; i <= 5 ; i++){
+            setMatriz(0, i, "Adicionar pagina");
+            setMatriz(1, i, "Pagina vazia");
+            setMatriz(2, i, "Normal");
+        }
+        
+    }
+    
 
     public static void menu(){
         int num = Integer.parseInt(JOptionPane.showInputDialog(null, "Bem-vindo ao iPhone!"+
@@ -170,21 +178,26 @@ public class Main {
         }
     }
 
-    public static void visuallizarPaginas(){
-        int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Viauslizar Páginas:"+
-        for(int i = 0; i <=6 ; i++){
-            String pag = getMatriz(1, i);
-            return "\n" + i+ " " + pag + ;
+    public static String listaPaginas() {
+        String lista = "";
+        for(int i = 0; i <= 5; i++) {
+            String pag = getMatriz(0, i);
+            lista += "\n" + (i +1) + " - " + pag;
         }
-        "\n0 - Voltar"));
+        return lista;
+    }
+
+
+    public static void visualizarPaginas(){
+        int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Visualizar Páginas:"+ listaPaginas() + "\n0 - Voltar"));
 
         switch (opcao) {
-            case 1: visualizador(1); break;
-            case 2: visualizador(2); break;
-            case 3: visualizador(3); break;
-            case 4: visualizador(4); break;
-            case 5: visualizador(5); break;
-            case 6: visualizador(6); break;
+            case 1: visualizador(0); break;
+            case 2: visualizador(1); break;
+            case 3: visualizador(2); break;
+            case 4: visualizador(3); break;
+            case 5: visualizador(4); break;
+            case 6: visualizador(5); break;
             case 0: navegador(); return;
             default:
                 JOptionPane.showMessageDialog(null, "Opção inválida!");
@@ -194,6 +207,76 @@ public class Main {
     }
 
     public static void visualizador(int pag){
-        
+
+        if("Adicionar pagina".equals(paginas[0][pag])) {
+            String pagina = JOptionPane.showInputDialog(null, "Digite o nome da pagina: ");
+            setMatriz(0, pag, pagina);
+            String conteudo = JOptionPane.showInputDialog(null, "Digite o conteudo da pagina: ");
+            setMatriz(1, pag, conteudo);
+
+            visualizador(pag);
+        } else {
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, paginas[0][pag] +
+            "\n" +
+            "\n" + paginas[1][pag]+
+            "\n" +
+            "\n1 - Atualizar pagina"+
+            "\n2 - Adicionar aos favoritos"+
+            "\n0 - Voltar"));
+
+            switch (opcao) {
+                case 1: atualizar(pag); break;
+                case 2: favoritar(pag); break;
+                case 0: visualizarPaginas(); return;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida!");
+                    visualizador(pag);
+                    return;
+            }
+        }
+
+    }
+
+    public static void atualizar(int pag){
+        String pagina = JOptionPane.showInputDialog(null, "Digite o novo nome da pagina: ");
+        setMatriz(0, pag, pagina);
+        String conteudo = JOptionPane.showInputDialog(null, "Digite o novo conteudo da pagina: ");
+        setMatriz(1, pag, conteudo);
+
+        visualizador(pag);
+    }
+
+    public static void  favoritar(int pag){
+        setMatriz(2, pag, "Favorito");
+
+        navegador();
+    }
+
+    public static void verFavoritos(){
+        String lista = "";
+        for (int i = 0; i <= 5; i++) {
+            String pag = getMatriz(0, i);
+            if("Favorito".equals(getMatriz(2, i))) { 
+                lista += "\n" + (i +1) + " - " + pag;
+            }
+        }
+
+        int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Favoritos:"+
+        "\n" + lista +
+        "\n0 - Voltar"));
+
+        switch (opcao) {
+            case 1: visualizador(0); break;
+            case 2: visualizador(1); break;
+            case 3: visualizador(2); break;
+            case 4: visualizador(3); break;
+            case 5: visualizador(4); break;
+            case 6: visualizador(5); break;
+            case 0: navegador(); return;
+            default:
+                JOptionPane.showMessageDialog(null, "Opção inválida!");
+                navegador();
+                return;
+        }
     }
 }
